c1c609056b71c470d246ceea9cc3dc1e517fe959
Rh4096 <rhelvetican@gmail.com>
2025-08-08 12:20:22 +0700

    "initialization"

--- src/bin/dg.rs
+++ src/bin/dg.rs
@@ -1,4 +1,8 @@
-use std::{error::Error, fs::File, io::Read};
+use std::{
+    error::Error,
+    fs::{create_dir_all, File},
+    io::Read,
+};
 
 use dagger::{load_dagger_lua_api, Directories};
 use mlua::Lua;
@@ -7,11 +11,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let lua = Lua::new();
     load_dagger_lua_api(&lua)?;
 
-    if let Ok(mut f) = File::open(Directories::config_dir()) {
+    let mut script = Directories::config_dir();
+    let _ = create_dir_all(&script);
+
+    println!("{}", script.display());
+
+    script.push("init.lua");
+
+    if let Ok(mut f) = File::open(&script) {
+        println!("Lua script found. Executing...");
+
         let mut buf = Vec::new();
         f.read_to_end(&mut buf)?;
         lua.load(&buf).exec()?;
     }
+
+    println!("{}", Directories::mod_dir().display());
 
     Ok(())
 }
--- src/directories/mod.rs
+++ src/directories/mod.rs
@@ -1,6 +1,13 @@
 use std::path::PathBuf;
 
-use dirs::{config_dir, data_dir};
+#[cfg(target_os = "linux")]
+mod linux;
+#[cfg(target_os = "macos")]
+mod macos;
+#[cfg(target_os = "windows")]
+mod windows;
+
+use dirs::{config_local_dir, data_dir};
 
 #[derive(Default, Clone, Copy)]
 pub struct Directories;
@@ -8,7 +15,7 @@ pub struct Directories;
 #[cfg(any(target_os = "windows", target_os = "linux", target_os = "macos"))]
 impl Directories {
     fn config() -> PathBuf {
-        unsafe { config_dir().unwrap_unchecked() }
+        unsafe { config_local_dir().unwrap_unchecked() }
     }
 
     fn data() -> PathBuf {
@@ -24,21 +31,5 @@ impl Directories {
 
     fn data() -> PathBuf {
         compile_error!("Unsupported platform. Only Windows, Linux or MacOs are supported.")
-    }
-}
-
-impl Directories {
-    /// Get the Dagger configuration directory.
-    pub fn config_dir() -> PathBuf {
-        let mut dir = Self::config();
-        dir.push("/Dagger/");
-        dir
-    }
-
-    /// Get the Balatro mod directory.
-    pub fn mod_dir() -> PathBuf {
-        let mut dir = Self::data();
-        dir.push("/Balatro/Mods/");
-        dir
     }
 }
